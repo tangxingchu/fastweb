26f86dc56710ad70d78fa181fef5f608eca2807e
tangxingchu <51697550@qq.com>
2017-08-25 14:22:22 +0800

    m

--- app/components/container.js
+++ app/components/container.js
@@ -141,43 +141,51 @@ export default class Container extends Component {
 		});
 		return (
 			<Row>
-				<Col span={18} style={styles.dashedCol}>
-					<sampleNameSpace.icon type='retweet'/>
-					<Button onClick={()=>{}}/>
-					{this.state.children.map((item, index)=> {
-						let Component = modules[item.metaType];
-						return (<Component {...item}>row{index} c</Component>);
-					})}
-					<Complete/>
-				</Col>
-				<Col span={6}>
-					<Menu
-						onClick={this.handleClick}
-						selectedKeys={['mail']}
-						mode="horizontal"
-					  >
-						<Menu.Item key="mail">
-						  <Icon type="mail" />Navigation One
-						</Menu.Item>
-						<Menu.Item key="app" disabled>
-						  <Icon type="appstore" />Navigation Two
-						</Menu.Item>
-						<SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-						  <MenuItemGroup title="Item 1">
-							<Menu.Item key="setting:1">Option 1</Menu.Item>
-							<Menu.Item key="setting:2">Option 2</Menu.Item>
-						  </MenuItemGroup>
-						  <MenuItemGroup title="Item 2">
-							<Menu.Item key="setting:3">Option 3</Menu.Item>
-							<Menu.Item key="setting:4">Option 4</Menu.Item>
-						  </MenuItemGroup>
-						</SubMenu>
-						<Menu.Item key="alipay">
-						  <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-						</Menu.Item>
-					  </Menu>
-					  
-				</Col>
+				<Row>
+					<Col span={18} style={styles.dashedCol}>
+						<sampleNameSpace.icon type='retweet'/>
+						<Button onClick={()=>{}}/>
+						{this.state.children.map((item, index)=> {
+							let Component = modules[item.metaType];
+							return (<Component {...item}>row{index} c</Component>);
+						})}
+						<Complete/>
+					</Col>
+					<Col span={6}>
+						<Menu
+							onClick={this.handleClick}
+							selectedKeys={['mail']}
+							mode="horizontal"
+						  >
+							<Menu.Item key="mail">
+							  <Icon type="mail" />Navigation One
+							</Menu.Item>
+							<Menu.Item key="app" disabled>
+							  <Icon type="appstore" />Navigation Two
+							</Menu.Item>
+							<SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+							  <MenuItemGroup title="Item 1">
+								<Menu.Item key="setting:1">Option 1</Menu.Item>
+								<Menu.Item key="setting:2">Option 2</Menu.Item>
+							  </MenuItemGroup>
+							  <MenuItemGroup title="Item 2">
+								<Menu.Item key="setting:3">Option 3</Menu.Item>
+								<Menu.Item key="setting:4">Option 4</Menu.Item>
+							  </MenuItemGroup>
+							</SubMenu>
+							<Menu.Item key="alipay">
+							  <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+							</Menu.Item>
+						  </Menu>
+						  
+					</Col>
+				</Row>
+				<Row>
+					<Col span={18}>
+						<div style={{height: 'calc(100% - 140px)', backgroundColor:'#efefef', border: '1px solid #ccc'}}></div>	
+					</Col>
+					<Col span={6}></Col>
+				</Row>
 			</Row>
 		)
 	}
